e1c33906204fbade41023aa7542b2837b942c3c4
Andrea Cronin <andrea@andreacroninsells.com>
2021-03-30 22:20:27 -0500

    Created Palette Component

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Palette extends Component {
+    render () {
+        return (
+        <div>
+            <div className='Palette'>
+                {/* Navbar goes here */}
+                <div className='Palette-colors'>{/*bunch of colors*/}</div>
+                    {/* Footer Eventually */}
+            </div>
+        </div>
+        )
+    }
+}
+export default Palette;
