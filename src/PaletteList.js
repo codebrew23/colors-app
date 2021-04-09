d8542c080150968bce4dec964ca3697646cd3166
Andrea Cronin <andrea@andreacroninsells.com>
2021-04-08 21:52:12 -0500

    jss styles with demo styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,13 @@
 import react, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './miniPalette'
 
 class PaletteList extends Component {
     render () {
         const {  palettes } = this.props;
         return (
             <div>
+                <MiniPalette />
                 <h1>React Colors</h1>
                 {palettes.map(palette => (
                     <p>
