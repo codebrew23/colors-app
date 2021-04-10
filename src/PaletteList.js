e7003723ab6ee09f5c096e091d6f5e38e7b7909c
Andrea Cronin <andrea@andreacroninsells.com>
2021-04-09 21:23:50 -0500

    Add link to palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,8 +1,6 @@
 import react, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 import MiniPalette from './miniPalette'
 import { withStyles } from '@material-ui/styles';
-import { PlayCircleFilledWhite } from '@material-ui/icons';
 
 const styles = {
     root: {
@@ -35,6 +33,9 @@ const styles = {
     }
 };
 class PaletteList extends Component {
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`)
+    }
     render () {
         const { palettes, classes } = this.props;
         return (
@@ -45,7 +46,7 @@ class PaletteList extends Component {
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette => (
-                        <MiniPalette {...palette} />
+                        <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)}/>
                         ))}
                     </div>
                 </div>
--- src/miniPalette.js
+++ src/miniPalette.js
@@ -1,60 +1,63 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
-import { grey } from '@material-ui/core/colors';
+import styles from "./styles/MiniPaletteStyles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-const styles = {
-    root: {
-        backgroundColor: 'white',
-        border: '1px solid black',
-        borderRadius: '5px',
-        padding: '0.5rem',
-        position: 'relative',
-        overflow: 'hidden',
-        '&:hover':{
-            cursor: 'pointer'
-        }
-    },
-    colors: {
-        backgroundColor: '#dae1e4',
-        height: '150px',
-        width: '100%',
-        borderRadius: '5px',
-        overflow: 'hidden'
-    },
-    title: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        margin:'0',
-        color: 'black',
-        paddingTop: '0.5rem',
-        fontSize: '1rem',
-        position: 'relative'
-    },
-    emoji: {
-        marginLeft: '0.5rem',
-        fontSize: '1.5rem'
-    },
-    miniColor: {
-        height: '25%',
-        width: '20%',
-        display: 'inline-block',
-        margin: '0 auto',
-        position: 'relative',
-        marginBottom: '-3.5px'
-    }
-};
+// const styles = {
+//     root: {
+//         backgroundColor: 'white',
+//         border: '1px solid black',
+//         borderRadius: '5px',
+//         padding: '0.5rem',
+//         position: 'relative',
+//         overflow: 'hidden',
+//         '&:hover':{
+//             cursor: 'pointer'
+//         }
+//     },
+//     colors: {
+//         backgroundColor: '#dae1e4',
+//         height: '150px',
+//         width: '100%',
+//         borderRadius: '5px',
+//         overflow: 'hidden'
+//     },
+//     title: {
+//         display: 'flex',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         margin:'0',
+//         color: 'black',
+//         paddingTop: '0.5rem',
+//         fontSize: '1rem',
+//         position: 'relative'
+//     },
+//     emoji: {
+//         marginLeft: '0.5rem',
+//         fontSize: '1.5rem'
+//     },
+//     miniColor: {
+//         height: '25%',
+//         width: '20%',
+//         display: 'inline-block',
+//         margin: '0 auto',
+//         position: 'relative',
+//         marginBottom: '-3.5px'
+//     }
+// };
+
 function MiniPalette(props) {
-    const { classes, paletteName, emoji, colors} = props;
-    console.log('colors: ', colors);
+    
+    const { classes, paletteName, emoji, colors } = props;
     const miniColorBoxes = colors.map(color => (
         <div className={classes.miniColor} 
              style={{backgroundColor: color.color}}
              key={color.name}>
         </div>
     ))
+
     return(
-        <div className={classes.root}>
+        <div className={classes.root} onClick={props.handleClick}>
             <div className={classes.colors}>
                 {miniColorBoxes}
             </div>
@@ -64,4 +67,5 @@ function MiniPalette(props) {
         </div>
     )
 }
+
 export default withStyles(styles) (MiniPalette); //higher order Component
