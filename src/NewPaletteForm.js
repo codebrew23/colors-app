2cfcb2669007dce094a3e850968a21bfc94d78c2
Andrea Cronin <andrea@andreacroninsells.com>
2021-04-17 15:48:57 -0500

    refactored navigation to its own component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Button from '@material-ui/core/Button'
 import { ChromePicker } from 'react-color'
 import DraggableColorList from './DraggableColorList';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { arrayMove } from 'react-sortable-hoc';
-
+import PaletteFormNav from './paletteFormNav';
 
 
 const drawerWidth = 400;
@@ -91,7 +87,6 @@ class NewPaletteForm extends Component {
        currentColor: 'teal',
        newColorName: '',
        colors: this.props.palettes[0].colors,
-       newPaletteName: ''
     };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
@@ -138,11 +133,10 @@ class NewPaletteForm extends Component {
 
     this.setState ({ colors: [...this.state.colors, newColor ], newColorName: '' });
   }
-  handleSubmit() {
-    let newName=this.state.newPaletteName;
+  handleSubmit(newPaletteName) {
     const newPalette = {
-      paletteName: newName,
-      id: newName.toLowerCase().replace(/ /g, '-'),
+      paletteName: newPaletteName,
+      id: newPaletteName.toLowerCase().replace(/ /g, '-'),
       colors: this.state.colors
     };
     this.props.savePalette(newPalette);
@@ -173,51 +167,18 @@ class NewPaletteForm extends Component {
   }
 
   render(e) {
-    const { classes, maxColors } = this.props;
+    const { classes, maxColors, palettes } = this.props;
     const { open, colors } = this.state;
     const paletteIsFull = colors.length >= maxColors;
 
     return (
       <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          color= 'default'
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Persistent drawer
-            </Typography>
-            <ValidatorForm onSubmit={this.handleSubmit}> 
-              <TextValidator
-                label='Palette Name' 
-                value={this.state.newPaletteName}
-                name='newPaletteName'
-                onChange={this.handleChange}
-                validators={['required', 'isPaletteNameUnique']}
-                errorMessages={['enter palette name', 'this palette name is in use']}
-              />
-              <Button 
-                variant="contained" 
-                color="primary"
-                type='submit'
-              >
-                Save Palette
-              </Button>
-            </ValidatorForm>
-          </Toolbar>
-        </AppBar>
+        <PaletteFormNav 
+          open={open} 
+          classes={classes} 
+          palettes={palettes}
+          handleSubmit = {this.handleSubmit}
+          handleDrawerOpen = {this.handleDrawerOpen}/>
         <Drawer
           className={classes.drawer}
           variant="persistent"
